55a5ddc35392f3376539c6d2b62cc156aa70a948
John Fiewor <johnfiewor@gmail.com>
2021-09-14 15:00:02 +0100

    fix textarea responsiveness

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
     <DropArea/>
-    <Footer/>
+    {/* <Footer/> */}
     </div>
   );
 }
--- src/components/DropArea.jsx
+++ src/components/DropArea.jsx
@@ -1,62 +1,28 @@
-import React, { useState } from "react"
-// import PublishIcon from '@material-ui/icons/Publish';
-import IconButton from '@material-ui/core/IconButton';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import React, {useState} from "react"
+import PublishIcon from '@material-ui/icons/Publish';
+import Button from "@material-ui/core/Button";
 
 const DropArea = () => {
-    // function displayContent(){
-        
-        // }
-        // {
-        //     const file = document.getElementById('uploadForm')
-        //     console.log(file.value)
-        // }
+    const [file, setFile] = useState('')
+    
+    function fileUpload(event){
+        setFile(event.target.value)
+        console.log("working")
+    }
 
-        const [file, setFile] = useState('')
-        // let fileInput = document.getElementById('uploadForm')
-        // let files = fileInput.files
-        // let file
-        
-        function fileUpload(event){
-            const value = event.target.value;
-            setFile(value)
-            console.log(file)
-            // files.map(file=>{
-            //     return (
-            //         file.name
-            //     )
-            // }) 
-        }
+    function sendFiles(){
+        console.log("upload button was clicked")
+    }
 
-        return (
-            <div>
-                <form className="upload-area" id="uploadForm">
-                    <textarea readonly="yes" name="content" id="" cols="20" rows="3" placeholder="Drop images here">
-                        {/* <PublishIcon /> */}
-                    </textarea>
-                    <input onChange={fileUpload} type="file" name="uploadfile" multiple/>
-                    {/* <IconButton className="upload-icon" color="primary" aria-label="upload picture" component="span">
-                        <PhotoCamera style={{ fontSize: 60 }}/>
-                    </IconButton> */}
-                </form>
-            </div>
-        )
-        // {
-        //     files.map(file=>{
-        //         return (
-        //             file.name;
-        //         )
-        //     })
-        // }
-        // {
-        //     let fileInput = document.querySelector('#uploadForm')
-        //     let files = fileInput.files
-        //     let file
-        //     for(let i= 0; i < file.length; i++){
-        //         file = files.item(i)
-        //         alert(file.name)
-        //     }
-        // }
+    return (
+        <div>
+            <form action="post" className="upload-area" id="uploadForm">
+                <textarea readonly="yes" name="content" id="" cols="20" rows="3" placeholder="Upload images here"/>
+                <input onChange={fileUpload} type="file" name="uploadfile" value={file} multiple/>
+                <Button onClick={sendFiles} className="upload-icon-container"><PublishIcon className="upload-icon"/></Button>
+            </form>
+        </div>
+    )
 }
 
 export default DropArea;
