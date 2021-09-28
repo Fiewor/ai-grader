a987962a1c8fc69377faa039029b05f25475ebf5
John Fiewor <johnfiewor@gmail.com>
2021-09-28 23:49:07 +0100

    use axios instead of fetch in attempt to solve error

--- src/components/DropArea.jsx
+++ src/components/DropArea.jsx
@@ -1,13 +1,15 @@
 import React, {useState} from "react"
-import PublishIcon from '@material-ui/icons/Publish';
-import Button from "@material-ui/core/Button";
+import PublishIcon from '@material-ui/icons/Publish'
+import Button from "@material-ui/core/Button"
+import axios from "axios"
 
 const DropArea = () => {
     const [files, setFiles] = useState('')
-
     
     function fileUpload(event){
         setFiles(Array.from(event.target.files))
+        console.log(files)
+        // setFiles(Array(event.target.files))
     }
     
     function sendFiles(event){
@@ -17,26 +19,30 @@ const DropArea = () => {
         const formData = new FormData()
 
         files.forEach((fileItem, i) => {
+            console.log(fileItem)
             formData.append(i, fileItem)
         })
 
-        const requestOptions = {
-            mode: "no-cors",
-            method: "POST",
-            body: formData
-        }
+        // const requestOptions = {
+            // mode: "no-cors",
+        //     method: "POST",
+        //     body: formData
+        // }
         
-        fetch("http://localhost:3001/upload", requestOptions)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        .catch(error => console.error(error))
+        // fetch("http://localhost:3001/upload", requestOptions)
+        // .then(response => response.json())
+        // .then(data => console.log(data))
+
+        axios.post("http://localhost:3001/upload", formData)
+        .then(res => console.log(res))
+        .catch(error => console.log(error))
     }
 
     return (
         <div>
             <form className="upload-area">
-                <textarea readonly="yes" name="content" id="" cols="20" rows="3" placeholder="Upload images here"/>
-                <input onChange={fileUpload} type="file" multiple/>
+                <textarea readOnly="yes" name="content" id="" cols="20" rows="3" placeholder="Upload images here"/>
+                <input onChange={fileUpload} type="file" name="file" multiple/>
                 <Button type="submit" onClick={sendFiles} className="upload-icon-container"><PublishIcon className="upload-icon"/></Button>
             </form>
         </div>
