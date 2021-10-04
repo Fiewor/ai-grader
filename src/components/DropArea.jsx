d9df5c03fb9355c56f4e646a3a875c178213ca9f
John Fiewor <johnfiewor@gmail.com>
2021-10-04 12:41:41 +0100

    remove request options and fix upload functionality

--- src/components/DropArea.jsx
+++ src/components/DropArea.jsx
@@ -9,29 +9,17 @@ const DropArea = () => {
     function fileUpload(event){
         setFiles(Array.from(event.target.files))
         console.log(files)
-        // setFiles(Array(event.target.files))
     }
     
     function sendFiles(event){
         event.preventDefault()
 
-        // attempt to upload file using FormData() and fetch() per the docs: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
         const formData = new FormData()
 
         files.forEach((fileItem, i) => {
             console.log(fileItem)
             formData.append(i, fileItem)
         })
-
-        // const requestOptions = {
-            // mode: "no-cors",
-        //     method: "POST",
-        //     body: formData
-        // }
-        
-        // fetch("http://localhost:3001/upload", requestOptions)
-        // .then(response => response.json())
-        // .then(data => console.log(data))
 
         axios.post("http://localhost:3001/upload", formData)
         .then(res => console.log(res))
