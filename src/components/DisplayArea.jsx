c41bd67ad33ca070831f8154db63fbd8a6ddf8ae
Fiewor John <johnfiewor@gmail.com>
2022-03-10 13:14:54 +0100

    feat: add loading screen with lottie animation

--- src/components/DisplayArea.jsx
+++ src/components/DisplayArea.jsx
@@ -1,32 +1,47 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import { Player } from "@lottiefiles/react-lottie-player";
 
 const DisplayArea = () => {
   const [text, setText] = useState([]);
-  //! TO-DO: after grading logic is done, display result of grading
+  const [loading, setLoading] = useState(undefined);
+  const [completed, setCompleted] = useState(undefined);
 
+  //! TO-DO: after grading logic is done, display result of grading
   useEffect(() => {
     const getText = async () => {
       let result = await axios.get(`http://localhost:3001/viewText`);
       setText(result.data);
+      setLoading(true);
+
+      if (result) setCompleted(true);
     };
     getText();
-  }, [text.length]);
+  }, [text]);
 
   console.log("data", text);
 
   return (
     <>
-      {text.map((result) => {
-        return (
+      {!loading ? (
+        <Player
+          autoplay
+          loop
+          mode="normal"
+          src="https://assets1.lottiefiles.com/datafiles/HN7OcWNnoqje6iXIiZdWzKxvLIbfeCGTmvXmEm1h/data.json"
+          speed="1"
+          style={{ width: "70%" }}
+        ></Player>
+      ) : (
+        text.map((result) => (
           <div>
             <h2>{result.readText}</h2>
             {result.keyPhrases.map((item) => (
               <p>{item}</p>
             ))}
           </div>
-        );
-      })}
+        ))
+      )}
     </>
   );
 };
