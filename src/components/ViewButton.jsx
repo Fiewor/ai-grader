17ec361208253a8b248827dc0315234d53d34975
Fiewor <johnfiewor@gmail.com>
2022-11-05 18:33:06 +0100

    style: reduce padding and change text color

--- src/components/ViewButton.jsx
+++ src/components/ViewButton.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
+export const ViewButton = ({ children, path }) => {
+  return (
+    <UploadButton>
+      <Link to={path}>{children}</Link>
+    </UploadButton>
+  );
+};
+
 const UploadButton = styled.button`
   background: #959595;
   padding: 0.01rem 1.5rem;
   border: none;
   margin: 0.2rem 0.5rem;
-  border-radius: 20px;
-  box-shadow: 1px 1px 1px 1px rgb(0, 0, 0);
+  border-radius: 5px;
+  box-shadow: 1px 2px 1px 1px rgb(0, 0, 0);
   @media screen and (min-width: 768px) {
     font-size: 1rem;
   }
@@ -21,16 +29,8 @@ const UploadButton = styled.button`
   }
   a {
     text-decoration: none;
-    color: white;
+    color: black;
     padding: 0;
     margin: 0;
   }
 `;
-
-export const ViewButton = ({ children, path }) => {
-  return (
-    <UploadButton>
-      <Link to={path}>{children}</Link>
-    </UploadButton>
-  );
-};
