cdec31fd495c51e6f75d73b50bbf70707b2117fd
Fiewor <johnfiewor@gmail.com>
2023-01-25 22:57:15 +0100

    chore: add reactquery dependency

--- src/index.js
+++ src/index.js
@@ -8,13 +8,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./App";
 import "./index.scss";
+
+const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Router>
   </React.StrictMode>,
   document.getElementById("root")
