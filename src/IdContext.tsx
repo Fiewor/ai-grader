854308b1a87e300f74fcd8011d58f4488d511685
Fiewor <johnfiewor@gmail.com>
2023-02-02 12:59:53 +0100

    fix: correctly populate/depopulate markId/answerId and add responsive check for answer/mark click

--- src/IdContext.tsx
+++ src/IdContext.tsx
@@ -3,7 +3,8 @@ import React, { createContext, useReducer, Dispatch, FC } from "react";
 interface IdState {
   markIds: string[];
   answerIds: string[];
-  isChecked: boolean;
+  selectedAnswerSheet: boolean;
+  selectedMarkSheet: boolean;
 }
 
 interface Props {
@@ -19,7 +20,8 @@ interface IAction {
 const initialIds: IdState = {
   markIds: [],
   answerIds: [],
-  isChecked: false,
+  selectedAnswerSheet: false,
+  selectedMarkSheet: false,
 };
 
 export const IdContext = createContext<IdState | null>(null);
@@ -43,25 +45,39 @@ export const IdProvider: FC<Props> = ({ children }) => {
 
 function idReducer(ids: IdState, action: IAction) {
   switch (action.type) {
-    case "CHECKED": {
-      return {
-        ...ids,
-        isChecked: !ids.isChecked,
-      };
-    }
-
+    // this action is used to add or remove an id from the markIds or answerIds array
     case "TOGGLE_STORE": {
-      if (action.key === "markIds" || action.key === "answerIds") {
+      if (action.key === "answerIds" || action.key === "markIds") {
         const key = action.key as "markIds" | "answerIds";
+        // if action.id is in markIds, remove it, otherwise add it
         return {
           ...ids,
-          [key]: ids.isChecked
-            ? [...ids[key], action.id]
-            : ids[key].filter((val) => val !== action.id),
+          [key]: ids[key].includes(action.id as string)
+            ? ids[key].filter((val) => val !== action.id)
+            : [...ids[key], action.id],
         };
       } else {
         throw new Error(
           "The 'key' property of the action must be either 'markIds' or 'answerIds'"
+        );
+      }
+    }
+
+    // this action is used to set the selectedAnswerSheet or selectedMarkSheet to true or false based on the length of the markIds or answerIds array i.e. if the user has selected answers or marks
+    case "SET_CHECKED_SHEET": {
+      if (action.key === "answerIds" || action.key === "markIds") {
+        const arr = ids[action.key as "markIds" | "answerIds"];
+        const key =
+          action.key === "answerIds"
+            ? "selectedAnswerSheet"
+            : "selectedMarkSheet";
+        return {
+          ...ids,
+          [key]: arr.length > 0,
+        };
+      } else {
+        throw new Error(
+          "The 'key' property of the action must be either 'answerIds' or 'markIds'"
         );
       }
     }
