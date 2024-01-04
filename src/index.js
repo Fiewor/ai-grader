a462a4ddc7c0a1e0af84c2732dcaa105e3e27402
Fiewor <johnfiewor@gmail.com>
2024-01-04 21:00:54 +0100

    add suspense and error feat: boundary

--- src/index.js
+++ src/index.js
@@ -5,13 +5,14 @@
 // import "core-js/modules/es6.string.trim";
 // import "core-js/modules/es7.object.values";
 
-import React from "react";
+import React, {Suspense} from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import App from "./App";
 import "./index.scss";
+import ErrorBoundary from "./ErrorBoundary";
 
 const queryClient = new QueryClient();
 
@@ -19,7 +20,11 @@ ReactDOM.render(
   <React.StrictMode>
     <Router>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <ErrorBoundary>
+            <Suspense fallback="Loading...">
+              <App />
+            </Suspense>
+        </ErrorBoundary>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </Router>
